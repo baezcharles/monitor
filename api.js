e85a5701409662c1e78f26262913ed6ddfa3ca1c
Pavan <Pavan>
2017-06-24 20:16:57 +0530

    Migrated to postgres

--- api.js
+++ api.js
@@ -1,12 +1,25 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('data/monitor');
+// var sqlite3 = require('sqlite3').verbose();
+// var db = new sqlite3.Database('data/monitor');
 
-// Initialization
-db.serialize(function() {
-  console.log("Monitoring started");
-  db.run("CREATE TABLE IF NOT EXISTS counts (id INTEGER PRIMARY KEY ASC, username TEXT, totalCount INTEGER DEFAULT 1)");
-	//db.run("INSERT INTO counts (id, username, totalCount) VALUES (?, ?, ?)", 0, "pavan", 1);
-});
+var Pool = require('pg').Pool;
+
+var config = {
+  //host: 'localhost',
+  user: 'pavan',
+  password: 'pavan',
+  database: 'monitor',
+};
+
+process.on('unhandledRejection', function(e) {
+  console.log(e.message, e.stack)
+})
+
+var pool = new Pool(config)
+
+
+//pool.query("DROP TABLE IF EXISTS counts");
+pool.query("CREATE TABLE IF NOT EXISTS counts (id SERIAL, username TEXT, totalCount INTEGER DEFAULT 1)");
+
 var express = require('express');
 var restapi = express();
 var bodyParser = require('body-parser');
@@ -19,37 +32,29 @@ restapi.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 restapi.get('/list', function(req, res){
   console.log("list api called");
 	var result;
-	db.all("SELECT * FROM counts", function(err, rows) {
+	pool.query("SELECT * FROM counts", function(err, rows) {
       //console.log(JSON.stringify(rows));
-	  res.end(JSON.stringify(rows));
+	  res.end(JSON.stringify(rows.rows));
     });
-    /*db.each("SELECT * FROM notes", function(err, row){
-        //console.log(row.id + ": " + row.title + ": " + row.note);
-		console.log(row);
-		res.json(row);
-		//result += '{ "id" : "' + row.id + '", "title" : "' + row.title + '", "note" : "' + row.note + '"},';
-    });*/
-	//result = '[' + result + ']';
-	//console.log(result);
-	//res.json(result);
 });
 
-//To display number of requests received
 restapi.get('/', function(req, res){
   console.log("update api called");
-    db.run("UPDATE counts SET totalCount = totalCount + 1 WHERE username = ?", req.query.username, function(err, affectedRows){
-      if(!this.changes){
-        db.run("INSERT into counts(username, totalCount) values(?, 1)", req.query.username);
-      };
+  pool.query("UPDATE counts SET totalCount = totalCount + 1 WHERE username = $1", [req.query.username], function(err, affectedRows){
+      if (err) console.log(err);
+      if(!affectedRows.rowCount){
+        pool.query("INSERT into counts(username, totalCount) values($1, 1)", [req.query.username], function(err){
+          if (err) console.log(err);
+        });
+      }
     });
     res.json({ "status" : "done" });
 });
 
 restapi.get('/ca701625c04e4b73f8f761fa69b8dde7', function(req, res){
   console.log("delete api called");
-    db.run("DELETE FROM counts WHERE username = ?", req.query.username, function(err, affectedRows){
+    pool.query("DELETE FROM counts WHERE username = $1", [req.query.username], function(err, affectedRows){
       console.log(err);
-      console.log(this.changes);
     });
     res.json({ "status" : "done" });
 });
